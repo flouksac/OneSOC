de86c65942294effe873cc13a82166e0b17dfa34
Flouksac <102966875+flouksac@users.noreply.github.com>
2025-02-17 11:19:42 +0100

    fix filebeat creds

--- src/Controller/ControllerService/wazuh_server_controller.py
+++ src/Controller/ControllerService/wazuh_server_controller.py
@@ -316,6 +316,8 @@ class Wazuh_Server_Controller(AbstractComponentServiceController):
 
             # Ajout des ips des indexer wazuh
             config["output.elasticsearch"]["hosts"] = ast.literal_eval(self._get_option("list-of-indexers-ip",True).value)
+            config["output.elasticsearch"]["username"] = self._get_option("indexer-username",True).value
+            config["output.elasticsearch"]["password"] = self._get_option("indexer-password",True).value
 
             self.view.display(f"filebeat.yml file updated with the provided settings :", context="info", indent=2, level=3)
             self.view.display_pretty_dict(config, level=3, indent=2)
